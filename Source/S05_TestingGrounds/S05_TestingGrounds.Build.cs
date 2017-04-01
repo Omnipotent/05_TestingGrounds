f3d1c616203891e9f04a68a5bd91d8dbfedf2fa6
Nicholas Wilkie <io2red@hotmail.com>
2017-04-01 15:50:43 -0700

    Enabled Git LFS & Added FPS Content

---
+++ Source/S05_TestingGrounds/S05_TestingGrounds.Build.cs
@@ -0,0 +1,11 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class S05_TestingGrounds : ModuleRules
+{
+	public S05_TestingGrounds(TargetInfo Target)
+	{
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+	}
+}
---
+++ Source/S05_TestingGrounds/S05_TestingGroundsGameMode.h
@@ -0,0 +1,16 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+#pragma once
+#include "GameFramework/GameModeBase.h"
+#include "S05_TestingGroundsGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AS05_TestingGroundsGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AS05_TestingGroundsGameMode();
+};
+
+
+
