845588666c4b137b9650f4320de522d819f17e34
Nicholas Wilkie <io2red@hotmail.com>
2017-04-21 09:42:43 -0700

    TG69 - Merging Our TP and FP Files

--- Source/S05_TestingGrounds/S05_TestingGroundsGameMode.cpp
+++ Source/S05_TestingGrounds/S05_TestingGroundsGameMode.cpp
@@ -9,7 +9,7 @@ AS05_TestingGroundsGameMode::AS05_TestingGroundsGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Player/Behavior/FirstPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Dynamic/Character/Behavior/Character"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
 	// use our custom HUD class
