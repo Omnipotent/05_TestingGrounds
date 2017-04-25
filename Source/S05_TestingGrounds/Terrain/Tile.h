5f36e2f695275c9a8b0ac5755e3b77292ce31214
Nicholas Wilkie <io2red@hotmail.com>
2017-04-25 16:17:32 -0700

    TG83 - Generating Random Numbers

---
+++ Source/S05_TestingGrounds/Terrain/Tile.h
@@ -0,0 +1,30 @@
+// Copyright Nicholas Wilkie 2017
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "Tile.generated.h"
+
+UCLASS()
+class S05_TESTINGGROUNDS_API ATile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ATile();
+
+	UFUNCTION(BlueprintCallable, Category = "Weapon")
+	void PlaceActors();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	
+	
+};
