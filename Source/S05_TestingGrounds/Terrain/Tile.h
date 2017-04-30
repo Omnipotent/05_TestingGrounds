171ef10d3c7811afb4758596dd837c60e41becf3
Nicholas Wilkie <io2red@hotmail.com>
2017-04-30 09:00:52 -0700

    TG96 - Structs to Simplify Function Args

--- Source/S05_TestingGrounds/Terrain/Tile.h
+++ Source/S05_TestingGrounds/Terrain/Tile.h
@@ -5,6 +5,16 @@
 #include "GameFramework/Actor.h"
 #include "Tile.generated.h"
 
+USTRUCT()
+struct FSpawnPosition
+{
+	GENERATED_USTRUCT_BODY()
+
+	FVector Location;
+	float Rotation;
+	float Scale;
+};
+
 class UActorPool;
 
 UCLASS()
@@ -37,16 +47,17 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
-
 	UFUNCTION(BlueprintCallable, Category = "Pool")
 	void SetPool(UActorPool* Pool);
 
 private:
 	void PositionNavMeshBoundsVolume();
 
+	TArray<FSpawnPosition> RandomSpawnPositions(int MinSpawn, int MaxSpawn, float Radius, float MinScale, float MaxScale);
+
 	bool FindEmptyLocation(FVector& OutLocation, float Radius);
 
-	void PlaceActor(TSubclassOf<AActor> ToSpawn, FVector SpawnPoint, float Rotation, float Scale);
+	void PlaceActor(TSubclassOf<AActor> ToSpawn, FSpawnPosition SpawnPosition);
 
 	bool CanSpawnAtLocation(FVector Location, float Radius);
 	
